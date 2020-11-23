f9e10fd3a579fcfe1f6c5a6514500b52780686fa
Lindsay Zhou <zhou.lindsay@yahoo.com>
2020-11-23 18:33:55 -0500

    Updates

---
+++ precache-manifest.b37a3a35b43afe70ea2c4461e2d00848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702dda40f03879d952b7c62334104995",
+    "url": "/personal-page-generator/index.html"
+  },
+  {
+    "revision": "91ed1d7c01aab716e6b1",
+    "url": "/personal-page-generator/static/css/main.c95ff7ca.chunk.css"
+  },
+  {
+    "revision": "642882388ef2ac534f61",
+    "url": "/personal-page-generator/static/js/2.d84fd9c1.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/personal-page-generator/static/js/2.d84fd9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ed1d7c01aab716e6b1",
+    "url": "/personal-page-generator/static/js/main.0a9927bc.chunk.js"
+  },
+  {
+    "revision": "0e2d5ac8d455c8194ae2",
+    "url": "/personal-page-generator/static/js/runtime-main.fcfa5c98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-page-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
